e901bd2477df819491f297f24ed38a205bfd8b0b
Ivan <ivanfbj@gmail.com>
2022-12-08 16:45:54 -0500

    Ruta para retornar elementos
    Se crea la ruta para retornar toda la lista de elementos del archivo "products.py"

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 # Crear ruta de prueba para validar el funcionamiento del servidor para}
 # Cuando el navegador realice la petición a la ruta /ping va a dar la respuesta del return
 # De esta manera se puede testear que el servidor este respondiento con algo al navegador.
+# Lasa rutas funcionan por defecto con la ruta GET en caso de no especificar al atributo "methods"
 @app.route('/ping')
 def pingt():
     return jsonify({"message":"Servidor activo, se le retorna al navegador un mensaje en formato JSON."})
+
+# Con la ruta "/products" lo que podemos hacer es retornar la lista de productos que tenemos en el archivo products.py
+# Se puede retornar directamente la lista o por medio de una propiedad o se puede incluir otra propiedad.py
+# Los ejemplo quedan comentareados en la función.
+@app.route('/products', methods=['GET'])
+def getProducts():
+    # return jsonify(products)
+    # return jsonify({"products": products})
+    return jsonify({"products": products,
+                    "message": "Product's List"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
