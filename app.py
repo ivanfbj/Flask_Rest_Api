2260efaf62879494f6c76b2c95ffb1e510c81015
Ivan <ivanfbj@gmail.com>
2022-12-08 15:44:37 -0500

    Prueba de testeo de servidor activo
    Se verifica que el servidor si responda la petición del navegador.

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from products import products
 
 app = Flask(__name__)
+
+# Crear ruta de prueba para validar el funcionamiento del servidor para}
+# Cuando el navegador realice la petición a la ruta /ping va a dar la respuesta del return
+# De esta manera se puede testear que el servidor este respondiento con algo al navegador.
+@app.route('/ping')
+def pingt():
+    return 'Pong!'
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
